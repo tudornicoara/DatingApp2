a6f1a7adff58f034fe068cc8814c8610524a22be
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-06-18 11:21:24 +0300

    API Exception Handling

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -1,4 +1,5 @@
 using DatingApp.Extensions;
+using DatingApp.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -41,16 +42,7 @@ namespace DatingApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
