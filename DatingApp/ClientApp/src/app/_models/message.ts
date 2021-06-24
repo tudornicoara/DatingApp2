a4ef79d89e84aed76de98b05a0caa71c162af9d6
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-06-24 13:48:12 +0300

    Added messaging function

---
+++ DatingApp/ClientApp/src/app/_models/message.ts
@@ -0,0 +1,12 @@
+﻿export interface Message {
+  id: string;
+  senderId: string;
+  senderUsername: string;
+  senderPhotoUrl: string;
+  recipientId: string;
+  recipientUsername: string;
+  recipientPhotoUrl: string;
+  content: string;
+  dateRead?: Date;
+  messageSent: Date;
+}
---
+++ DatingApp/Interfaces/IMessageRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.Dtos;
+using DatingApp.Entities;
+using DatingApp.Helpers;
+
+namespace DatingApp.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(Guid id);
+        Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
+        Task<bool> SaveAllAsync();
+    }
+}
