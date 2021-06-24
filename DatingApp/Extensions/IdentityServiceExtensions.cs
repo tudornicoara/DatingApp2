7c29b593f8ba1e93a66de4a63d9ed2f32e2563e0
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-06-24 17:26:11 +0300

    Roles and dmin component

--- DatingApp/Extensions/IdentityServiceExtensions.cs
+++ DatingApp/Extensions/IdentityServiceExtensions.cs
@@ -35,6 +35,12 @@ namespace DatingApp.Extensions
                     };
                 });
 
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
             return services;
         }
     }
