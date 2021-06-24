4637820cd6139104db3418a61b5796cce58d57a9
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-06-24 17:00:46 +0300

    Rolse

--- DatingApp/Interfaces/ITokenService.cs
+++ DatingApp/Interfaces/ITokenService.cs
@@ -1,9 +1,10 @@
-﻿using DatingApp.Entities;
+﻿using System.Threading.Tasks;
+using DatingApp.Entities;
 
 namespace DatingApp.Interfaces
 {
     public interface ITokenService
     {
-        string CreateToken(AppUser user);
+        Task<string> CreateToken(AppUser user);
     }
 }
