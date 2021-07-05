4fa0d228cc209415eb03a0b1d31e951adaf7b295
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-07-05 14:19:23 +0300

    Auto scroll down for messages

--- DatingApp/ClientApp/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp/ClientApp/src/app/members/member-messages/member-messages.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Message} from "../../_models/message";
 import {MessageService} from "../../_services/message.service";
 import {NgForm} from "@angular/forms";
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css']
