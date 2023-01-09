8beeb9dca1260ff9705a4a613e6e78914b1f72df
Tudor Nicoara <tudor.nicoara@endava.com>
2023-01-09 14:22:04 +0200

    Changed the loading spinner

--- DatingApp/ClientApp/src/app/_services/busy.service.ts
+++ DatingApp/ClientApp/src/app/_services/busy.service.ts
@@ -12,7 +12,7 @@ export class BusyService {
   busy() {
     this.busyRequestCount++;
     this.spinnerService.show(undefined, {
-      type: 'ball-spin-clockwise-fade-rotating',
+      type: 'line-scale-party',
       bdColor: 'rgba(255,255,255,0)',
       color: '#333333'
     });
