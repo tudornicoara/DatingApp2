0d6e2c7f49e79125373ea80fbb319ceb5c35310a
Tudor Nicoara <tudor.nicoara@endava.com>
2023-01-09 14:09:39 +0200

    Added a fallback controller

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -36,9 +36,13 @@ app.UseCors("CorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
